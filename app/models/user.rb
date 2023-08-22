affe85b8fb56417efb4d587d68cd57032f32799c
farhanahmed0308 <farhanahmed2297@gmail.com>
2023-08-22 23:55:38 +0500

    authentication_and_authorization

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :Trackable, :validatable
+         has_and_belongs_to_many :books
+end
